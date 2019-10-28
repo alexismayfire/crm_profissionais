b2588d9b867e4336cab3c799f65804a71c49c610
Rodrigo Moroni <rodrigomoroni@hotmail.com>
2019-10-28 17:44:41 -0300

    Tela de Login

--- web/src/index.js
+++ web/src/index.js
@@ -1,12 +1,19 @@
+// Import libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Login from './Login'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create react component
+const App = () => {
+    return (
+        <div>
+            <Login></Login>
+        </div>
+    );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Render react component
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
