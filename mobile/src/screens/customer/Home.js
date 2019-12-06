48d32e55a00aff405a30dff56659ab8c6ecaf786
Rodrigo Moroni <rodrigomoroni@hotmail.com>
2019-12-06 21:48:00 -0200

    Home do Customer com botão para procurar profissionais próximos, imprime as coordenadas

--- mobile/src/screens/customer/Home.js
+++ mobile/src/screens/customer/Home.js
@@ -1,10 +1,5 @@
-/*import React from 'react';
-import { View } from 'react-native';
-import { Text } from 'react-native-elements';
-
-export default props => <View><Text>Customer Home</Text></View>;*/
 import React, { Component } from 'react';
-import { Platform, Text, View, StyleSheet } from 'react-native';
+import { Platform, Text, View, StyleSheet, Button } from 'react-native';
 import Constants from 'expo-constants';
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
@@ -12,24 +7,29 @@ import * as Permissions from 'expo-permissions';
 export default class Home extends Component {
   state = {
     location: null,
-    errorMessage: null,
+    text: '',
   };
 
-  componentWillMount() {
-    if (Platform.OS === 'android' && !Constants.isDevice) {
-      this.setState({
-        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
-      });
-    } else {
-      this._getLocationAsync();
+  displayLocation = async () => {
+    if (this.state.location == null){
+      if (Platform.OS === 'android' && !Constants.isDevice) {
+        this.setState({
+          text: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+        });
+      } else {
+        await this._getLocationAsync();
+      }
     }
+    this.setState({
+      text: JSON.stringify(this.state.location)
+    })
   }
 
   _getLocationAsync = async () => {
     let { status } = await Permissions.askAsync(Permissions.LOCATION);
     if (status !== 'granted') {
       this.setState({
-        errorMessage: 'Permission to access location was denied',
+        text: 'Permission to access location was denied',
       });
     }
 
@@ -38,16 +38,10 @@ export default class Home extends Component {
   };
 
   render() {
-    let text = 'Waiting..';
-    if (this.state.errorMessage) {
-      text = this.state.errorMessage;
-    } else if (this.state.location) {
-      text = JSON.stringify(this.state.location);
-    }
-
     return (
       <View style={styles.container}>
-        <Text style={styles.paragraph}>{text}</Text>
+        <Button onPress={this.displayLocation} title='Buscar profissionais próximos'/>
+        <Text style={styles.paragraph}>{this.state.text}</Text>
       </View>
     );
   }
